7fabd656ba1b8508b9280c9c7f1792d481df2ce9
HarithaV99 <harithaneha09@gmail.com>
2024-09-22 17:17:31 -0400

    Base Implementations

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>College Events Platform</h1>
+            <p>Coming soon: List of upcoming events...</p>
+        </div>
+    );
+};
+
+export default App;
